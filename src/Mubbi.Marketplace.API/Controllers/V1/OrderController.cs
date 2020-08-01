03c2b1dd461fb5872bbdfbc0fab260ce9f0b1f6e
Felipe de Almeida <felipe.allmeida.dev@gmail.com>
2020-08-01 01:24:06 -0300

    created order controller

---
+++ src/Mubbi.Marketplace.API/Controllers/V1/OrderController.cs
@@ -0,0 +1,108 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mubbi.Marketplace.API.Controllers.V1.Attributes;
+using Mubbi.Marketplace.API.Models;
+using Mubbi.Marketplace.Infrastructure.Bus.Communication;
+using Mubbi.Marketplace.Infrastructure.Bus.Messages.DomainNotifications;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mubbi.Marketplace.API.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    [ValidateModel]
+    [ApiController]
+    public class OrderController : ApiController
+    {
+        public OrderController(INotificationHandler<DomainNotification> notifications, IMediatorHandler mediator, IMapper mapper)
+            : base(notifications, mediator, mapper) { }
+        
+        [HttpGet]
+        [Route("")]
+        [SwaggerOperation(Summary = "Get all orders", Description = "Get a list of all orders")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]
+        public async Task<ActionResult> GetALl(
+            [SwaggerParameter("The Id of a user", Required = false)][FromQuery] Guid? userId,
+            [SwaggerParameter("The page to be displayed", Required = false)][FromQuery] int? currentPage,
+            [SwaggerParameter("The max number of pages that should be returned, the default value is 50", Required = false)][FromQuery] int? pageSize,
+            [SwaggerParameter("If the product should be sorted by property, the default value is sort property is 'Id'", Required = false)][FromQuery] string sortBy,
+            [SwaggerParameter("If the sort order should be ascendant or descendant, the default value is descendant", Required = false)][FromQuery] string sortOrder
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("")]
+        [SwaggerOperation(Summary = "Create Order", Description = "Create a new user order")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]        
+        public async Task<ActionResult> CreateProduct([FromBody] CreateOrderViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [SwaggerOperation(Summary = "Update Order", Description = "Update a existing order")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]
+        public async Task<ActionResult> UpdateOrder([FromRoute] Guid id, [FromBody] UpdateOrderViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("{id}/voucher")]
+        [SwaggerOperation(Summary = "Apply voucher", Description = "Apply a voucher to the order")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]
+        public async Task<ActionResult> ApplyVoucher([FromRoute] Guid id, [FromBody] ApplyVoucherViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [SwaggerOperation(Summary = "Delete a order", Description = "Delete a existing order.")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]
+        public async Task<ActionResult> Delete([FromRoute] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [SwaggerOperation(Summary = "Delete a voucher ", Description = "Delete a existing voucher applyied to a order.")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResponse<List<string>>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResponse<List<string>>))]
+        public async Task<ActionResult> DeleteVoucher([FromRoute] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
